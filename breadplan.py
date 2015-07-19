59f826a3360e466beea5805a5e06dede6b6b5a9a
Hawk Weisman <hi@hawkweisman.me>
2015-07-19 12:04:40 -0400

    PEP8-ify some formatting

--- breadplan.py
+++ breadplan.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3.4
+from docopt import docopt
+from tabulate import tabulate
+
 doc = """BreadPlan
 
 Usage:
@@ -14,70 +17,89 @@ Options:
     -s --sugar                      Whether or not to add sugar for the crust.
 """
 
-from docopt import docopt
-from tabulate import tabulate
-
-WATER           = 240 # Assume water has a density of 1 gram
-SALT            = 273
-TBSP_PER_CUP    = 16
+WATER        = 240  # Assume water has a density of 1 gram
+SALT         = 273
+TBSP_PER_CUP = 16
 
 # See http://nbviewer.ipython.org/github/hawkw/breadplan/blob/master/Breadplan%20Science.ipynb
 # for the math behind these density values.
-BREAD_FLOUR          = 127.33333333333333
-ALL_PURPOSE          = 125.0
-WHOLE_WHEAT          = 122.0
-RYE                  = 103.66666666666667
-FIFTY_FIFTY          = (BREAD_FLOUR + WHOLE_WHEAT)/2
+BREAD_FLOUR = 127.33333333333333
+ALL_PURPOSE = 125.0
+WHOLE_WHEAT = 122.0
+RYE         = 103.66666666666667
+FIFTY_FIFTY = (BREAD_FLOUR + WHOLE_WHEAT)/2
 
 water_grams_to_cup = lambda grams: round(grams / WATER, 1)
 flour_grams_to_cup = lambda grams, flour: round(grams/flour, 1)
-leaven_grams_to_cup = lambda grams: round(
-                                            (
-                                                ((grams/2) / FIFTY_FIFTY) + ((grams/2) / WATER)
-                                            )/2,
-                                            1
-                                        )
-salt_grams_to_tbsp = lambda grams: round(
-                                            (grams / SALT) / TBSP_PER_CUP,
-                                            1
-                                        )
+leaven_grams_to_cup = lambda grams: round((((grams/2) / FIFTY_FIFTY) +
+                                           ((grams/2) / WATER))/2, 1)
+salt_grams_to_tbsp = lambda grams: round((grams / SALT) / TBSP_PER_CUP, 1)
 
-def basic_country(loaves = 2, hydration = 75, sugar=True):
+def basic_country(loaves=2, hydration=75, sugar=True):
+    water = water_grams_to_cup(375*loaves)
+    leaven = leaven_grams_to_cup(100*loaves)
+    white = flour_grams_to_cup(450*loaves, BREAD_FLOUR)
+    wheat = flour_grams_to_cup(50*loaves, WHOLE_WHEAT)
+    salt = salt_grams_to_tbsp(loaves * 10)
     table = [
-                ["Water (80F)", "{} cups".format(water_grams_to_cup(375*loaves)), "{:.0F}%".format(hydration)],
-                [ "Leaven", "{} cups".format(leaven_grams_to_cup(100*loaves)),"20%" ],
-                [ "White Flour","{} cups".format(flour_grams_to_cup(450*loaves,BREAD_FLOUR)),"90%" ],
-                [ "Whole Wheat Flour","{} cups".format(flour_grams_to_cup(50*loaves,WHOLE_WHEAT)),"10%" ],
-                [ "Salt", "{} Tbsp".format(salt_grams_to_tbsp(loaves * 10)),"2%" ]
+                ["Water (80F)",
+                    "{} cups".format(water), "{:.0F}%".format(hydration)],
+                ["Leaven",
+                    "{} cups".format(leaven), "20%"],
+                ["White Flour",
+                    "{} cups".format(white), "90%"],
+                ["Whole Wheat Flour",
+                    "{} cups".format(wheat), "10%"],
+                ["Salt",
+                    "{} Tbsp".format(salt), "2%"]
             ]
 
     if sugar:
         table.append(["Sugar", "{} Tbsp".format(.5*loaves)])
 
-    print ("Recipe for {l:.0F} loaves of Basic Country Bread:\n".format(l = loaves))
-    print (tabulate(table, headers = ["Ingredient", "Quantity", "%"]))
+    print ("Recipe for {l:.0F} loaves of Basic Country Bread:\n"
+            .format(l=loaves))
+    print (tabulate(table, headers=["Ingredient", "Quantity", "%"]))
 
-def whole_wheat(loaves = 2, hydration = 80, sugar=True):
+
+def whole_wheat(loaves=2, hydration=80, sugar=True):
+    water = water_grams_to_cup(400*loaves)
+    leaven = leaven_grams_to_cup(100*loaves)
+    wheat = flour_grams_to_cup(450*loaves, WHOLE_WHEAT)
+    all_purp = flour_grams_to_cup(50*loaves, ALL_PURPOSE)
+    salt = salt_grams_to_tbsp(loaves * 10)
     table = [
-                ["Water (80F)", "{} cups".format(water_grams_to_cup(400*loaves)), "{:.0F}%".format(hydration)],
-                [ "Leaven", "{} cups".format(leaven_grams_to_cup(100*loaves)),"20%" ],
-                [ "Whole Wheat Flour","{} cups".format(flour_grams_to_cup(450*loaves,WHOLE_WHEAT)),"70%" ],
-                [ "All Purpose Flour","{} cups".format(flour_grams_to_cup(50*loaves,ALL_PURPOSE)),"30%" ],
-                [ "Salt", "{} Tbsp".format(salt_grams_to_tbsp(loaves * 10)),"2%" ]
+                ["Water (80F)",
+                    "{} cups".format(water), "{:.0F}%".format(hydration)],
+                ["Leaven",
+                    "{} cups".format(leaven), "20%"],
+                ["Whole Wheat Flour",
+                    "{} cups".format(wheat), "70%"],
+                ["All Purpose Flour",
+                    "{} cups".format(all_purp), "30%"],
+                ["Salt", "{} Tbsp".format(salt), "2%"]
             ]
 
     if sugar:
         table.append(["Sugar", "{} Tbsp".format(.5*loaves)])
 
-    print ("Recipe for {l:.0F} loaves of Whole Wheat Bread:\n".format(l = loaves))
-    print (tabulate(table, headers = ["Ingredient", "Quantity", "%"]))
+    print ("Recipe for {l:.0F} loaves of Whole Wheat Bread:\n"
+           .format(l=loaves))
+    print (tabulate(table, headers=["Ingredient", "Quantity", "%"]))
 
 
 if __name__ == "__main__":
     opts = docopt(doc, help=True, version="0.1")
     if opts["basic"]:
-        basic_country(float(opts["--loaves"]), float(opts["--hydration"]), opts["--sugar"])
+        basic_country(
+            float(opts["--loaves"]),
+            float(opts["--hydration"]),
+            opts["--sugar"])
     elif opts["wholewheat"]:
-       whole_wheat(float(opts["--loaves"]), float(opts["--hydration"]), opts["--sugar"])
+        whole_wheat(
+            float(opts["--loaves"]),
+            float(opts["--hydration"]),
+            opts["--sugar"])
     else:
-        print ("Sorry, breads other than the Basic Country Bread are not currently supported")
+        print ("Sorry, breads other than the Basic Country Bread" +
+               "are not currently supported")
